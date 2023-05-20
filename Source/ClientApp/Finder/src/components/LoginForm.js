502679eb990abed74581d2e2eb745dbb5711060f
Mariia Verhun <mariia.verhun@gmail.com>
2023-05-20 16:15:37 +0300

    Login and register form without logic

--- Source/ClientApp/Finder/src/components/LoginForm.js
+++ Source/ClientApp/Finder/src/components/LoginForm.js
@@ -1,30 +1,71 @@
-import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { useDispatch } from "react-redux";
 import { login } from "../redux/actions/loginAction";
+import { Button } from "./MainButton";
+import { RegisterForm } from "./RegisterForm";
+
+const { width, height } = Dimensions.get("window");
+
+const RootStack = createNativeStackNavigator();
+
+const image = {
+  uri: "https://res.cloudinary.com/dhvntmrax/image/upload/v1684583252/kyiv_map_prytvk_8a6df1.jpg",
+};
 
 export function LoginForm() {
   const dispatch = useDispatch();
+  
 
-  async function getUserById(url){
+
+  async function getUserById(url) {
     const responce = await fetch(url);
-    return responce.json()
+    return responce.json();
   }
 
   const handleLogin = () => {
-    
     dispatch(login());
   };
 
-  return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <View>
-        <TextInput placeholder="Username" />
-        <TextInput placeholder="Password" />
+  function Form({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+          <View style={styles.loginForm}>
+            <Text style={styles.headingText}>Вхід</Text>
+            <TextInput style={styles.textInputs} placeholder="Email" />
+            <TextInput
+              style={styles.textInputs}
+              placeholder="Пароль"
+              secureTextEntry={true}
+            />
+            <View style={styles.buttonContainer}>
+              <Button title="Увійти" onPress={handleLogin}></Button>
+              <Button title="Реєстрація" onPress={() => navigation.navigate("Register")}></Button>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
-      <Button title="Login" onPress={handleLogin}></Button>
-    </View>
+    );
+  }
+
+  return (
+    <RootStack.Navigator
+      screenOptions={{ headerShown: false, headerTransparent: true }}
+    >
+      <RootStack.Group>
+        <RootStack.Screen name="Login" component={Form} />
+        <RootStack.Screen name="Register" component={RegisterForm} />
+      </RootStack.Group>
+    </RootStack.Navigator>
   );
 }
 
@@ -34,5 +75,45 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    overflow: "hidden",
+  },
+  loginForm: {
+    width: 0.8 * width,
+    height: 0.4 * height,
+    borderRadius: 50,
+    padding: 30,
+    alignItems: "stretch",
+    justifyContent: "center",
+    backgroundColor: "#00296ba9",
+  },
+  textInputs: {
+    fontFamily: "Monserrat-SemiBold",
+    borderColor: "#00296b",
+    borderWidth: 1,
+    fontSize: 16,
+    padding: 3,
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    width: width,
+    height: height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headingText: {
+    fontFamily: "Monserrat-SemiBold",
+    fontSize: 20,
+    paddingHorizontal: 5,
+    color: "#fff",
+    alignSelf: "center",
+    margin: 10,
+  },
+  buttonContainer: {
+    flexDirection: "column",
   },
 });
---
+++ Source/ClientApp/Finder/src/components/RegisterForm.js
@@ -0,0 +1,135 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  Pressable,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import { useDispatch } from "react-redux";
+import { login } from "../redux/actions/loginAction";
+import { Button } from "./MainButton";
+
+const { width, height } = Dimensions.get("window");
+
+export function RegisterForm({ navigation }) {
+  const dispatch = useDispatch();
+
+  async function getUserById(url) {
+    const responce = await fetch(url);
+    return responce.json();
+  }
+
+  const handleLogin = () => {
+    dispatch(login());
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Pressable
+          style={({ pressed }) =>
+            pressed ? styles.pressablePressed : styles.pressable
+          }
+          onPress={() => navigation.goBack()}
+        >
+          <Icon name="caret-left" size={35} color={"#ee6c4d"} />
+          <Text style={styles.headeringText}>Назад</Text>
+        </Pressable>
+      </View>
+      <View style={styles.loginForm}>
+        <Text style={styles.headingText}>Реєстрація</Text>
+        <TextInput style={styles.textInputs} placeholder="Ваше ім'я" />
+        <TextInput style={styles.textInputs} placeholder="Email" />
+        <TextInput
+          style={styles.textInputs}
+          placeholder="Пароль"
+          secureTextEntry={true}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="Зареєструватися" onPress={handleLogin}></Button>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  loginForm: {
+    padding: 30,
+    width: width,
+    height: 0.78 * height,
+    alignItems: "stretch",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  textInputs: {
+    fontFamily: "Monserrat-SemiBold",
+    borderColor: "#00296b",
+    borderWidth: 1,
+    fontSize: 16,
+    padding: 3,
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    width: width,
+    height: height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headingText: {
+    fontFamily: "Monserrat-SemiBold",
+    fontSize: 20,
+    paddingHorizontal: 5,
+    color: "#00296b",
+    alignSelf: "center",
+    margin: 10,
+  },
+  buttonContainer: {
+    flexDirection: "column",
+  },
+  header: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderBottomColor: "#878787",
+    borderBottomWidth: 0.75,
+  },
+  pressable: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  pressablePressed: {
+    opacity: 0.5,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  headeringText: {
+    padding: 10,
+    fontSize: 17,
+    lineHeight: 21,
+    fontFamily: "Monserrat-SemiBold",
+    letterSpacing: 0.25,
+    color: "#09008e",
+    // color: "#ee6c4d"
+  },
+});
