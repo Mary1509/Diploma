37adafcb155dcba235edcfa42655e68dfaaf42b2
Mariia Verhun <mariia.verhun@gmail.com>
2023-05-27 17:17:51 +0300

    nearest refactor

--- Source/PyServer/controllers/miscController.py
+++ Source/PyServer/controllers/miscController.py
@@ -12,20 +12,22 @@ def index():
 def getNearestShelters():
     lat = request.args.get('lat', None)
     lng = request.args.get('lng', None)
-    query = text(f"""SELECT  latitude, longitude, ROUND(earth_distance(ll_to_earth({lat},{lng}),
-            ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-    FROM shelters
+    query = text(f"""SELECT  sh.id, latitude, longitude, concat(addresses."street", ', ', addresses."houseNumber") AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}), ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+    FROM shelters sh
+    INNER JOIN addresses ON "addressId" = addresses.id
     WHERE
-    earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-    AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000""")
+    earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+    AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000""")
     shelters = db.session.execute(query).all()
     shelters_coords = nearest.findNearest((lat, lng), shelters)
     res = []
     if len(shelters_coords) > 0:
         for coord in shelters_coords:
             dict = {
-                'latitude': coord[0],
-                'longitude': coord[1]
+                'id': coord[0],
+                'latitude': coord[1],
+                'longitude': coord[2],
+                'address': coord[3]
             }
             res.append(dict)
     return make_response(jsonify(res), 200)
@@ -44,76 +46,89 @@ def getNearestSheltersWithFilters():
         if len(purposes) > 0:
             purposes_str = ','.join(purposes)
             if not hasRamp is None:
-                 query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                                  ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                                  ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                                  FROM shelters
+                 query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                                  AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                                  FROM shelters sh
+                                  INNER JOIN addresses ON "addressId" = addresses.id
                                   WHERE
-                                  earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                                  AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
+                                  earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                                  AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
                                   AND "typeId" IN ({types_str})
                                   AND "purposeId" IN ({purposes_str})
                                   AND "hasRamp" = {hasRamp}""")
             else:
-                query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                                 ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                                 ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                                 FROM shelters
+                query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                                 AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                                 FROM shelters sh
+                                 INNER JOIN addresses ON "addressId" = addresses.id
                                  WHERE
-                                 earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                                 AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
+                                 earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                                 AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
                                  AND "typeId" IN ({types_str})
                                  AND "purposeId" IN ({purposes_str})""")
         elif not hasRamp is None:
-            query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                                              ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                                              ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                                              FROM shelters
-                                              WHERE
-                                              earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                                              AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
-                                              AND "typeId" IN ({types_str})
-                                              AND "hasRamp" = {hasRamp}""")
-        else:
-            query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                             ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                             ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                             FROM shelters
+            query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                             AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                             FROM shelters sh
+                             INNER JOIN addresses ON "addressId" = addresses.id
                              WHERE
-                             earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
+                             earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
+                             AND "typeId" IN ({types_str})
+                             AND "hasRamp" = {hasRamp}""")
+        else:
+            query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                             AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                             FROM shelters sh
+                             INNER JOIN addresses ON "addressId" = addresses.id
+                             WHERE
+                             earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
                              AND "typeId" IN ({types_str})""")
     elif len(purposes) > 0:
         purposes_str = ','.join(purposes)
         if not hasRamp is None:
-            query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                             ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                             ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                             FROM shelters
+            query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                             AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                             FROM shelters sh
+                             INNER JOIN addresses ON "addressId" = addresses.id
                              WHERE
-                             earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
+                             earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
                              AND "purposeId" IN ({purposes_str})
                              AND "hasRamp" = {hasRamp}""")
         else:
-            query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                             ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                             ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                             FROM shelters
+            query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                             AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                             FROM shelters sh
+                             INNER JOIN addresses ON "addressId" = addresses.id
                              WHERE
-                             earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
+                             earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                             AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
                              AND "purposeId" IN ({purposes_str})""")
     else:
-        query = text(f"""SELECT  latitude, longitude, "typeId", "purposeId", "hasRamp",
-                         ROUND(earth_distance(ll_to_earth({lat},{lng}),
-                         ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
-                         FROM shelters
-                         WHERE
-                         earth_box(ll_to_earth ({lat},{lng}), 1000) @> ll_to_earth (latitude, longitude)
-                         AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 1000
-                         AND "hasRamp" = {hasRamp}""")
-    print(query)
+        query = text(f"""SELECT  sh.id, latitude, longitude, 
+                                          concat(addresses."street", ', ', addresses."houseNumber")
+                          AS address, ROUND(earth_distance(ll_to_earth({lat},{lng}),
+                                                    ll_to_earth(latitude, longitude))::NUMERIC, 2) AS distance
+                          FROM shelters sh
+                          INNER JOIN addresses ON "addressId" = addresses.id
+                          WHERE
+                          earth_box(ll_to_earth ({lat},{lng}), 10000) @> ll_to_earth (latitude, longitude)
+                          AND earth_distance(ll_to_earth ({lat},{lng}), ll_to_earth (latitude, longitude)) < 10000
+                          AND "hasRamp" = {hasRamp}""")
     shelters = db.session.execute(query).all()
     if len(shelters) > 0:
         shelters_coords = nearest.findNearest((lat, lng), shelters)
@@ -121,8 +136,11 @@ def getNearestSheltersWithFilters():
         if len(shelters_coords) > 0:
             for coord in shelters_coords:
                 dict = {
-                    'latitude': coord[0],
-                    'longitude': coord[1]
+                    'id': coord[0],
+                    'latitude': coord[1],
+                    'longitude': coord[2],
+                    'address': coord[3],
+                    'distance': coord[4]
                 }
                 res.append(dict)
         return make_response(jsonify(res), 200)
--- Source/PyServer/services/nearest.py
+++ Source/PyServer/services/nearest.py
@@ -6,16 +6,14 @@ from scipy import spatial
 def findNearest(location, shelters):
     shelers_tuples = []
     for shelter in shelters:
-        tup = shelter[0:2]
-        print(tup)
+        tup = shelter[1:3]
         shelers_tuples.append(tup)
 
     tree = spatial.KDTree(shelers_tuples)
     d, i = tree.query(location, k=6, p=2, eps=0)
-    print(d, i)
     result = []
     for ind in i:
-        if not d[ind] == float('inf'):
+        if not d[list(i).index(ind)] == float('inf'):
             tup = shelters[ind]
-            result.append(tup[:-1])
+            result.append(tup)
     return result
